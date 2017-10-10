8d9b296bca8bc0710ae6192718f603e8d615b657
purretao <purretao@gmail.com>
2017-10-10 12:50:41 +0200

    Nao AI improved a bit

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,40 @@
 const std::string ipnao = "128.39.75.111";
 AL::ALTextToSpeechProxy tts(ipnao, 9559);
 
+int win_possibility(Game g){
 
+	Player nao = g.get_turn();
+	Player p;
+	if (nao == RED) p = GREEN;
+	else p = RED; 
+	int i = -1;
+	for (int j = 0; j<7; j++){
+		i = g.get_first_empty_space(j);
+		if (i != -1){
+			g.set(i,j,Player_to_Board_state(nao));
+			if (g.is_over()) {
+			g.set(i,j,EMPTY);
+			return j;
+			}
+			g.set(i,j,EMPTY);			
+		}
+	}
+	
+	for (int j = 0; j<7; j++){
+		i = g.get_first_empty_space(j);
+		if (i != -1){
+			g.set(i,j,Player_to_Board_state(p));
+			if (g.is_over()) {
+			g.set(i,j,EMPTY);
+			return j;
+			}
+			g.set(i,j,EMPTY);
+		}
+	}
+	
+	return -1;
+
+}
 int main() {
 
   //Initialize the game, the human picks a color and who starts. 
@@ -41,8 +74,8 @@ int main() {
 
   std::cout << A << std::endl;
   int column;
-  int random;
-  
+  int ai_column;
+  int check;
   //After every turn, we check if the game is over. If it's not, we check if it's human turn or nao's turn. 
   while (!(A.is_over())) {
 
@@ -57,17 +90,19 @@ int main() {
       else {
       	A.apply(m);
       	std::cout << A << std::endl;
-      	after_human_turn(tts);
+      	if (!A.is_over()) after_human_turn(tts);
       }
     }
 
     else {
-      random = rand() % 7;
-      play_on_row(random, tts);
-      Move m(random, A.get_turn(), A);
+      check = win_possibility(A);
+      if(check != -1) ai_column = check; 
+      else ai_column = rand() % 7;
+      play_on_row(ai_column, tts);
+      Move m(ai_column, A.get_turn(), A);
       A.apply(m);
       std::cout << A << std::endl;
-      after_nao_turn(tts);
+      if (!A.is_over()) after_nao_turn(tts);
     }
   }
   Player Result = A.who_win();
